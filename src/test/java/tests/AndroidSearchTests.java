45fac44608a7b08147a0237a37024eebe914e114
TimeQA <qaeducation2022@gmail.com>
2022-11-30 02:36:09 +0300

    added attach

--- src/test/java/tests/AndroidSearchTests.java
+++ src/test/java/tests/AndroidSearchTests.java
@@ -5,16 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 
-public class AndroidSearchTests {
+public class AndroidSearchTests extends TestBase{
 
     @Test
     void searchTest() {
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
+        });
 
-        $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
-        $$(AppiumBy.className("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
-
+        step("Verify content found", () -> {
+            $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                    .shouldHave(sizeGreaterThan(0));
+        });
     }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestBase {
 
@@ -24,6 +23,7 @@ public class TestBase {
 
     @AfterEach
     public void afterEach() {
+        System.out.println(sessionId());
         closeWebDriver();
     }
 }

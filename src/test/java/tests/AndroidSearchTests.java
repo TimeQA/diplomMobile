86d058487d3a2a66da690c16940fa03efb4d745c
TimeQA <qaeducation2022@gmail.com>
2022-11-30 00:43:06 +0300

    moved to testbase

--- src/test/java/tests/AndroidSearchTests.java
+++ src/test/java/tests/AndroidSearchTests.java
@@ -1,7 +1,5 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-import drivers.BrowserstackMobileDriver;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Test;
 
@@ -13,16 +11,10 @@ public class AndroidSearchTests {
 
     @Test
     void searchTest() {
-        Configuration.browser = BrowserstackMobileDriver.class.getName();
-        Configuration.browserSize = null;
-
-        open();
 
         $(AppiumBy.accessibilityId("Search Wikipedia")).click();
         $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("BrowserStack");
         $$(AppiumBy.className("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
 
-        closeWebDriver();
     }
-
 }
